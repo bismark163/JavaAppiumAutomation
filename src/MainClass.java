256c4e53f736140239c8eb3ea10871a1cb0b8ab5
Кирилл Максименков <kirill.maksimenkov@e-legion.com>
2019-02-22 17:47:52 +0300

    Bla bla bla

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,2 +1,8 @@
-public class MainClass {
-}
+public class MainClass
+{
+    public int getLocalNumber()
+    {
+        int number = 14;
+        return number;
+    }
+}qwefqwefqwefqwef
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,2 +1,16 @@
-public class MainClassTest {
-}
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+
+{
+    @Test
+    public void testGetLocalNumber()
+    {
+        int a = this.getLocalNumber();
+        System.out.println(a);
+
+        Assert.assertTrue("Number is not 14",a == 14);
+    }
+}wefwefqwefqwefqwefqwefqwefqwefqwefqwefqweffqwef
+qwefwef qwefqwe
